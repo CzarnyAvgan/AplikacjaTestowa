9d2677b6c976d67fb4c766325ae06d33a1412cae
Kacper Wysocki <kacper.wysocki@exaco.pl>
2020-01-22 18:07:25 +0100

    inicialCommit

---
+++ AplikacjaRekrutacyjna/AplikacjaRekrutacyjna/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AplikacjaRekrutacyjna
+//
+//  Created by Kacper Wysocki on 22/01/2020.
+//  Copyright © 2020 Kacper Wysocki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
